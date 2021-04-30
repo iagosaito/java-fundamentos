94a282ca2c5512adee560261f23f34eecdc82baf
iagosaito <iagosaito1999@gmail.com>
2021-04-29 23:01:49 -0300

    adding test about Parameter class

--- 13.java-reflection/src/br/com/iagosaito/javareflection/Jogador.java
+++ 13.java-reflection/src/br/com/iagosaito/javareflection/Jogador.java
@@ -2,10 +2,11 @@ package br.com.iagosaito.javareflection;
 
 public class Jogador extends Pessoa {
 
-    public Jogador(String nome, int idade, String clube, int golsMarcados) {
+    public Jogador(String id, String nome, int idade, String clube, int golsMarcados) {
         super(nome, idade);
         this.clube = clube;
         this.golsMarcados = golsMarcados;
+        this.id = id;
     }
 
     public Jogador(String clube, int golsMarcados) {
@@ -15,8 +16,13 @@ public class Jogador extends Pessoa {
 
     public Jogador() {}
 
+    private String id;
     private String clube;
     private int golsMarcados;
+
+    public String getId() {
+        return id;
+    }
 
     public int getGolsMarcados() {
         return golsMarcados;
--- 13.java-reflection/src/br/com/iagosaito/javareflection/PessoaController.java
+++
@@ -1,4 +0,0 @@
-package br.com.iagosaito.javareflection;
-
-public class PessoaController {
-}
