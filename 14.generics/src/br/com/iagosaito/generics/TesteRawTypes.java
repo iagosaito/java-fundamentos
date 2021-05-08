bc408e4cde1a6282c77a4ae5e88d35b9fe68a776
iagosaito <iagosaito1999@gmail.com>
2021-05-07 23:36:08 -0300

    implementation of raw types

--- 14.generics/src/br/com/iagosaito/generics/TesteRawTypes.java
+++ 14.generics/src/br/com/iagosaito/generics/TesteRawTypes.java
@@ -4,5 +4,14 @@ public class TesteRawTypes {
 
     public static void main(String[] args) {
         Box box = new Box(); // Isso é um raw type
+
+        Box rawBox = new Box<String>();
+        rawBox.set(1234);
+        System.out.println(rawBox.get());
+
+
+        Box<Integer> intBox = new Box();
+        intBox.set(123);
+        System.out.println(intBox.get());
     }
 }
