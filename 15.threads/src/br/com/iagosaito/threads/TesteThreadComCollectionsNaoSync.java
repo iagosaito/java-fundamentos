a1a157596a8a0b0bdfe180dbdaf4874934a0e135
iagosaito <iagosaito1999@gmail.com>
2021-08-14 15:17:18 -0300

    adding wait and notify for own collection

--- 15.threads/src/br/com/iagosaito/threads/TesteThreadComCollectionsNaoSync.java
+++ 15.threads/src/br/com/iagosaito/threads/TesteThreadComCollectionsNaoSync.java
@@ -1,39 +1,88 @@
 package br.com.iagosaito.threads;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TesteThreadComCollectionsNaoSync {
 
     public static void main(String[] args) throws InterruptedException {
 
-        final List<String> list = new ArrayList<>();
+        final Lista lista = new Lista();
 
-        for (int i = 0; i < 10; i++) {
-            new Thread(new ListaAdicionaElemento(list, i)).start();
+        for (int i = 0; i < 2; i++) {
+            new Thread(new ListaAdicionaElemento(lista, i)).start();
         }
 
-        Thread.sleep(3000);
-
-        list.forEach(System.out::println);
+        new Thread(new TarefaImprimirElemento(lista)).start();
     }
 
+    private static class TarefaImprimirElemento implements Runnable {
+
+        private final Lista lista;
+
+        public TarefaImprimirElemento(final Lista lista) {
+            this.lista = lista;
+        }
+
+        @Override
+        public void run() {
+
+            synchronized (lista) {
+                try {
+                    lista.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                for (int i = 0; i < lista.tamanho(); i++) {
+                    System.out.println(i + " - " + lista.pegaElemento(i));
+                }
+            }
+        }
+    }
 }
 
 class ListaAdicionaElemento implements Runnable {
 
-    private final List<String> list;
+    private final Lista list;
     private final int numeroDaThread;
 
-    ListaAdicionaElemento(final List<String> list, final int numeroDaThread) {
+    ListaAdicionaElemento(final Lista list, final int numeroDaThread) {
         this.list = list;
         this.numeroDaThread = numeroDaThread;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 10000; i++) {
-            list.add("Thread " + numeroDaThread + " - " + i);
+        for (int i = 0; i < 100; i++) {
+            list.adicionaElemento("Thread " + numeroDaThread + " - " + i);
         }
     }
+}
+
+class Lista {
+
+    private final String[] elementos = new String[200];
+    private int indice;
+
+    public synchronized void adicionaElemento(final String elemento) {
+        this.elementos[indice] = elemento;
+        this.indice++;
+
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (indice == elementos.length) {
+            this.notify();
+        }
+    }
+
+    public int tamanho() {
+        return this.elementos.length;
+    }
+
+    public String pegaElemento(int posicao) {
+        return this.elementos[posicao];
+    }
+
 }
