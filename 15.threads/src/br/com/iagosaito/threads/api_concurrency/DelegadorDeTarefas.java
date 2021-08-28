0ebec11508e9b4c52b752adf180b6a5b88141276
Iago Hisami Saito <iagosaito1999@gmail.com>
2021-08-28 17:05:42 -0300

    Refacting server class

--- 15.threads/src/br/com/iagosaito/threads/api_concurrency/DelegadorDeTarefas.java
+++ 15.threads/src/br/com/iagosaito/threads/api_concurrency/DelegadorDeTarefas.java
@@ -7,9 +7,11 @@ import java.util.Scanner;
 class DelegadorDeTarefas implements Runnable {
 
     private final Socket socket;
+    private final Server server;
 
-    public DelegadorDeTarefas(final Socket socket) {
+    public DelegadorDeTarefas(final Socket socket, Server server) {
         this.socket = socket;
+        this.server = server;
     }
 
     @Override
@@ -18,7 +20,23 @@ class DelegadorDeTarefas implements Runnable {
         try {
             try (Scanner scanner = new Scanner(socket.getInputStream())) {
                 while (scanner.hasNextLine()) {
-                    System.out.println(scanner.nextLine());
+                    final String comando = scanner.nextLine();
+
+                    switch (comando) {
+                        case "c1":
+                            System.out.println("Enviando comando c1");
+                            break;
+                        case "c2":
+                            System.out.println("Enviando comando c2");
+                            break;
+                        case "fim":
+                            System.out.println("Encerrando servidor");
+                            server.finalizarServer();
+                            break;
+                        default:
+                            System.out.println("Comando aleatório");
+                            break;
+                    }
                 }
             }
             Thread.sleep(Duration.ofSeconds(5).toMillis());
--- 15.threads/src/br/com/iagosaito/threads/api_concurrency/Server.java
+++ 15.threads/src/br/com/iagosaito/threads/api_concurrency/Server.java
@@ -1,25 +1,44 @@
 package br.com.iagosaito.threads.api_concurrency;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 class Server {
-    public static void main(String[] args) throws Exception {
 
+    private final ServerSocket serverSocket;
+    private final ExecutorService threadPool;
+    private boolean deveRodar;
+
+    public Server() throws IOException {
         System.out.println("----- Iniciando servidor -------");
-        ServerSocket serverSocket = new ServerSocket(12345);
+        this.serverSocket = new ServerSocket(12345);
+        this.threadPool = Executors.newCachedThreadPool();
+        this.deveRodar = true;
+    }
 
-        final ExecutorService threadPool = Executors.newCachedThreadPool();
-    /*
-        final ExecutorService threadPool = Executors.newCachedThreadPool();
-        Quantidade fixa de Threads...
-     */
-
-        while (true) {
-            final Socket socket = serverSocket.accept();
-            threadPool.execute(new DelegadorDeTarefas(socket));
+    public void executarServidor() throws IOException {
+        while (this.deveRodar) {
+            System.out.println("Rodando...");
+            final Socket socket = this.serverSocket.accept();
+            this.threadPool.execute(new DelegadorDeTarefas(socket, this));
         }
     }
+
+    public void finalizarServer() throws IOException {
+        this.deveRodar = false;
+        this.serverSocket.close();
+        this.threadPool.shutdown();
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Server server = new Server();
+
+        server.executarServidor();
+        server.finalizarServer();
+    }
+
 }
